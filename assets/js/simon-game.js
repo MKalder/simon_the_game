2a0bd5920378bf33e7bc7e55f9bdfea2018c6018
Marius Kalder <147047923+MKalder@users.noreply.github.com>
2026-05-09 12:22:53 +0700

    adding lit and cleanups

--- assets/js/simon-game.js
+++ assets/js/simon-game.js
@@ -21,32 +21,38 @@ startBtn.addEventListener("click", startGame);
 function startGame() {
 
     console.log("game start 🚀");
-    playStartJingle();
+    //playStartJingle();
     state.playing = true;
 
     // DISABLE SETTINGS
     document.querySelectorAll(".toggle-card").forEach(card => {
-        console.log(card);
+        //console.log(card);
         card.classList.add("disabled");
     });
 
     // GAME LOOP
     while (state.playing) {
-        console.log("while loop");
 
-        //INFO TEXT
-        //Round X - start
+        //INFO TEXT UDATES
+        state.level = state.level + 1;
+        const gameInfo = document.querySelector(".game-info");
+        gameInfo.textContent = "Round " + state.level + " starts";
+        gameInfo.style.color = "var(--green-lit)";
 
-        //UPDATE Level
+        // LEVEL UPDATE
+        const levelInfo = document.querySelector(".level>p");
+        levelInfo.textContent = state.level;
 
-        //PLAY SEQUENCE
+        //CREATE PLAY SEQUENCE
+        simonSequence();
+        console.log(state.sequence);
+        simonSays();
 
         //PLAYER HAS TO REPLAY
+        playerRepeat();
 
-        //EXIT STATEMENT 
-        state.playing = false;
+        //UPDATE INFO GRAPHICS
     }
-
 
 }
 
@@ -60,6 +66,59 @@ function playStartJingle() {
 }
 
 function simonSequence() {
+    const COLORS = ["green", "red", "yellow", "blue"];
+    const randColor = Math.floor(Math.random() * 4);
+    //console.log(randColor);
+    state.sequence.push(COLORS[randColor]);
+}
+
+function simonSays() {
+    const TONES = { green: 329.6, red: 220, yellow: 261.6, blue: 164.8 };
+    const seconds = state.speedMode ? 500 : 1000;
+    const litDuration = seconds * 0.6;
+
+    for (let i = 0; i < state.sequence.length; i++) {
+        let sound = TONES[state.sequence[i]];
+        setTimeout(() => {
+            playTone(sound);
+            buttonLitOn(state.sequence[i]);
+
+            setTimeout(() => {
+                buttonLitOff(state.sequence[i]);
+            }, litDuration);
+
+        }, i * seconds);
+
+    }
+}
+
+function buttonLitOn(color) {
+    document.querySelector("." + color).classList.add("lit");
+}
+
+function buttonLitOff(color) {
+    document.querySelector("." + color).classList.remove("lit");
+}
+
+
+
+
+
+
+
+
+
+function playerRepeat() {
+
+    //If timer than switch on
+
+    const playBtn = document.querySelectorAll(".play-btn");
+    playBtn.forEach((btn, i) => {
+        btn.addEventListener("click", () => {
+            state.playerInput.push(i);
+            //compare function aufruf
+        });
+    });
 
 }
 
